6ab4bb221cd6e8c8e5b4402b5d3ab0eb486396e9
Baek Yongik <72693450+developer-caleb@users.noreply.github.com>
2024-10-12 22:30:09 +0900

    Flag game single play complete
    Flag game single play complete

--- Assets/0_Scenes/2_GAME_SCENE/Scenes/26_BlueWhiteFlag/GameManagerFlag.cs
+++ Assets/0_Scenes/2_GAME_SCENE/Scenes/26_BlueWhiteFlag/GameManagerFlag.cs
@@ -1,20 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Realtime;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManagerFlag : GameManager
 {
-    [SerializeField] AudioClip[] KrAudioClips;
+    // [SerializeField] AudioClip[] KrAudioClips;
 
     private AudioSource audioSource;
-    private int currentClipIndex = 0; // 현재 재생 중인 클립 인덱스
+
     private int numberRound = 10;
     private int nowRound = -1;
-
+    private int nowTeamAPoint = 0;
+    private int memberPoint = 10;
+    private int teamPoint = 50;
+    private int speedPoint = 30;
+    [SerializeField] TextMeshProUGUI PointCounter;
+    [SerializeField] TextMeshProUGUI RoundCounter;
+    PlayerFlag[] playerFlags;
+    public FlagRound nowFlagRound;
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
         StartCoroutine(ChangeStateAfterDelay(3f));
+        playerFlags = FindObjectsByType<PlayerFlag>(FindObjectsSortMode.None);
+        nowFlagRound = null;
     }
 
     void Update()
@@ -66,9 +78,84 @@ public class GameManagerFlag : GameManager
     }
     public override void GameStart()
     {
-        base.GameStart();
-        FlagRound[] flagRounds = new FlagRound[3];
+        // base.GameStart();
         Debug.Log("GameStart");
-        //GenerateRound();
+        FlagRound[] flagRounds = new FlagRound[numberRound];
+
+        // 각 인스턴스를 생성해 배열에 넣어야 함
+        for (int i = 0; i < flagRounds.Length; i++)
+        {
+            flagRounds[i] = new FlagRound(i);  // 배열의 각 원소에 인스턴스 할당
+        }
+        nowRound = 1;
+        StartCoroutine(GamePlay(flagRounds));
+    }
+    IEnumerator GamePlay(FlagRound[] flagRounds)
+    {
+        Debug.Log("GamePlay");
+        //UI변경,
+        for (int i = 0; i < flagRounds.Length; i++)
+        {
+            RoundCounter.text = "Round : " + nowRound;
+            Debug.Log($"{nowRound}번째 라운드 시작!");
+            nowFlagRound = flagRounds[i];
+            // 클립 재생 시작
+            yield return StartCoroutine(PlayClipsSequentially(flagRounds[i].roundClips, i));
+
+            // 2초 대기
+            yield return new WaitForSeconds(2);
+            CheckAnswerAndGetPoint();
+            foreach (PlayerFlag player in playerFlags)
+            {
+                player.Reset();
+            }
+
+            nowRound++;
+
+
+        }
+        yield return null;
+
+    }
+
+    private IEnumerator PlayClipsSequentially(AudioClip[] roundClips, int index)
+    {
+
+
+        // for 구문으로 변환
+        for (int j = 0; j < roundClips.Length; j++)
+        {
+            audioSource.clip = roundClips[j];
+            audioSource.Play();
+            Debug.Log($"{index}, {j} : PlayClipsSequentially");
+            // 현재 재생 중인 클립이 끝날 때까지 대기
+            while (audioSource.isPlaying)
+            {
+                yield return null;
+            }
+
+        }
+
+        // 모든 클립 재생 후 2초 대기
+        Debug.Log($"{index}, 모든 오디오 클립이 재생되었습니다. 2초 후 종료됩니다.");
+
+
+        Debug.Log("종료되었습니다.");
+    }
+    void CheckAnswerAndGetPoint()
+    {
+        PlayerFlag playerFlag = FindObjectOfType<PlayerFlag>();
+
+        if (nowFlagRound.AnswerBlueFlagState == playerFlag.blueFlagState && nowFlagRound.AnswerWhiteFlagState == playerFlag.whiteFlagState)
+        {
+            Debug.Log("정답입니다! +point50");
+            nowTeamAPoint += memberPoint;
+            PointCounter.text = nowTeamAPoint.ToString();
+        }
+        else
+        {
+            Debug.Log($"오답! +Answer: {nowFlagRound.AnswerBlueFlagState}, {nowFlagRound.AnswerWhiteFlagState}\n" +
+            $"You: {playerFlag.blueFlagState} , {playerFlag.whiteFlagState}");
+        }
     }
 }
