db4b0edf5fda96bacbc1ec82024fdb9f27cdb0ed
GoatKINGDONG <105641714+GoatKINGDONG@users.noreply.github.com>
2024-10-18 02:39:24 +0900

    땅이 튀어오름

--- Assets/0_Scenes/2_GAME_SCENE/Scenes/27_EatTheLand/00_Script/Ground_EatTheLand_Default.cs
+++ Assets/0_Scenes/2_GAME_SCENE/Scenes/27_EatTheLand/00_Script/Ground_EatTheLand_Default.cs
@@ -7,34 +7,38 @@ public class Ground_EatTheLand_Default : MonoBehaviour
     public Material[] materials;    
     private MeshRenderer _meshRender;
     private UpDownBoxCheck _upDownCheck;
+    private Rigidbody _rb;
 
     [SerializeField] private float _explsionForwardPower;
     [SerializeField] private float _explsionUpPower;
+    [SerializeField] private float _bounceForce;
+    [SerializeField] int _bouncePercent;
 
 
     private void Awake()
     {
         _meshRender = GetComponent<MeshRenderer>();
         _upDownCheck = GetComponent<UpDownBoxCheck>();
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
         if(_upDownCheck.detatchedObj)
         {
+            bool isIn = true;
             int index = (int)_upDownCheck.detatchedObj.GetComponent<PlayerControl>().team;
             _meshRender.material = materials[index];
-            //if (this.gameObject.CompareTag("EXPLOSION"))
+
+           
+           
+
+            this.gameObject.tag = "EXPLOSION";
+            Invoke("BeSpring", 0.1f);
+            //if (isIn)
             //{
-            //    foreach (var obj in _upDownCheck.detatchedObjs)
-            //    {
-            //        PlayerControl _control = obj.GetComponent<PlayerControl>();
-            //        //_control.explosionDir =
-            //        _control.ChangeState(EPlayer.EXPLOSION);
-            //        this.gameObject.tag = "Untagged";
-                 
-            //    }
+            //    BeSpring();
+            //    isIn= false;
             //}
             
         }
@@ -42,5 +46,35 @@ public class Ground_EatTheLand_Default : MonoBehaviour
         {
             return;
         }
+    }
+
+    private void BeSpring()
+    {
+        //int percent = Random.Range(1, 10);
+        //Debug.Log(percent);
+        Vector3 bounceDir = Vector3.up;
+
+        //this.gameObject.tag = "EXPLOSION";
+
+        _rb.AddForce(bounceDir * _bounceForce, ForceMode.Impulse);
+        this.gameObject.tag = "Untagged";
+        //if (percent <= _bouncePercent)
+        //{
+        //    Vector3 bounceDir = Vector3.up;
+
+        //    this.gameObject.tag = "EXPLOSION";
+        //    _rb.AddForce(bounceDir * _bounceForce, ForceMode.Impulse);
+        //    this.gameObject.tag = "Untagged";
+        //    //StartCoroutine(WaitForBeSpring());  
+        //}
+    }
+
+    IEnumerator WaitForBeSpring()
+    {
+        yield return new WaitForSeconds(1);
+        Vector3 bounceDir = Vector3.up;
+        this.gameObject.tag = "EXPLOSION";
+        _rb.AddForce(bounceDir * _bounceForce, ForceMode.Impulse);
+        this.gameObject.tag = "Untagged";
     }
 }
